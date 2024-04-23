9428bd3ba99ce3c9ea168040467c09b5508fdd7d
Venkat-Kancherla <devisri.21bce9122@vitapstudent.ac.in>
2024-04-23 21:45:22 +0530

    Bug issue

--- frontEnd/src/Pages/SignUp.jsx
+++ frontEnd/src/Pages/SignUp.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 
-import logo from '../assets/icons/logo.png';
+import logo from '../assets/images/logo.png';
 import { Link } from 'react-router-dom';
 
 const SignUp = () => {
